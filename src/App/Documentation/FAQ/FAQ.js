795e014f94cc27ba3e22afe08e89c412e25eef5f
haris799 <muhammad.sheikh93@yahoo.com>
2018-05-21 13:42:47 -0400

    Files

--- src/App/Documentation/FAQ/FAQ.js
+++ src/App/Documentation/FAQ/FAQ.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './FAQ.css'
 import { Link } from 'react-router-dom';
 
 const FAQ = () => {
     return (
         <div>
             <h1>FAQ Page</h1>
+            <div id="install-aws-outer">
+                <div className="install-aws-text">
+                    FeedBack:
+                </div>
+                <div className="feedBackBox">
+                    <textarea className="feedBackInput" rows="10" cols="20" name="usrtxt" wrap="hard"></textarea>
+                </div>
+                <input className="submitButton" type="submit" />
+            </div>
         </div>
     )
 }
